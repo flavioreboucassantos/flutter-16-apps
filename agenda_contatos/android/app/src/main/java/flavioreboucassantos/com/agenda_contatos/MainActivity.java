d996de38e4bd4a01653b804b4e88028efa4e4eeb
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-04-07 16:30:48 -0300

    Creates agenda_contatos project

---
+++ agenda_contatos/android/app/src/main/java/flavioreboucassantos/com/agenda_contatos/MainActivity.java
@@ -0,0 +1,6 @@
+package flavioreboucassantos.com.agenda_contatos;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ agenda_contatos/lib/helpers/contact_helper.dart
@@ -0,0 +1,44 @@
+import 'package:sqflite/sqflite.dart';
+
+const String contactTable = "contactTable";
+const String idColumn = "idColumn";
+const String nameColumn = "nameColumn";
+const String emailColumn = "emailColumn";
+const String phoneColumn = "phoneColumn";
+const String imgColumn = "imgColumn";
+
+class ContactHelper {}
+
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String img;
+
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img
+    };
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
+  }
+}
