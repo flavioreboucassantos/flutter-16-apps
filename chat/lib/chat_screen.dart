5bda13fb06d4da005053f97e004e93e7ff2ee1a4
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-04-20 18:21:47 -0300

    Updates chat project

--- chat/lib/chat_screen.dart
+++ chat/lib/chat_screen.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 
