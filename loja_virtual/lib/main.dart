7373759cf819a9caaa900c3fe49359f14f7b77d0
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-04-27 19:01:06 -0300

    Updates loja_virtual project

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter\'s Clothing',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      )
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
