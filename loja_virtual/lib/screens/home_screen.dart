5b8046f66a4487065122d5a60c101ca19a91877b
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-05-04 18:11:33 -0300

    Updates loja_virtual project

--- loja_virtual/lib/screens/home_screen.dart
+++ loja_virtual/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,8 +16,13 @@ class HomeScreen extends StatelessWidget {
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
         ),
-        Container(
-          color: Colors.red,
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Produtos'),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
         ),
         Container(
           color: Colors.yellow,
---
+++ loja_virtual/lib/tabs/products_tab.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/tiles/category_tile.dart';
+
+class ProductsTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<QuerySnapshot>(
+      future: FirebaseFirestore.instance.collection('products').get(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData)
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        else
+          return ListView(
+            children: ListTile.divideTiles(
+              tiles: snapshot.data.docs.map((doc) => CategoryTile(doc)),
+              color: Colors.grey[500],
+            ).toList(),
+          );
+      },
+    );
+  }
+}
