847cd1ad5235789a99c6925d9c1e32e182ffbf60
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-06-07 20:22:47 -0300

    Updates TriggerBuilder in the project loja_virtual

--- loja_virtual/lib/classes/trigger_builder.dart
+++ loja_virtual/lib/classes/trigger_builder.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 /// The last shared [safeContext] from [TriggerBuilder].
 BuildContext safeContext;
 
-/// Builds a [Widget] using a shared [safeContext] if it exists.
+/// Builds a [Widget] using the shared [safeContext] from [TriggerBuilder]
+/// if it exists.
 ///
 /// Author: flavioReboucasSantos@gmail.com
 class StatelessBuilder extends StatelessWidget {
