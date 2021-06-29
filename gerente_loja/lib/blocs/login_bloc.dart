81386eb6df7c853e1e12df5ebc8b5d61e39f87e4
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-06-29 20:11:19 -0300

    Updates gerente_loja project

--- gerente_loja/lib/blocs/login_bloc.dart
+++ gerente_loja/lib/blocs/login_bloc.dart
@@ -1,16 +1,31 @@
+import 'dart:async';
+
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gerente_loja/validators/login_validators.dart';
 import 'package:rxdart/rxdart.dart';
+
+enum LoginState {
+  IDLE,
+  LOADING,
+  SUCCESS,
+  FAIL,
+}
 
 class LoginBloc extends BlocBase with LoginValidators {
   final BehaviorSubject<String> _emailController = BehaviorSubject<String>();
   final BehaviorSubject<String> _passwordController = BehaviorSubject<String>();
+  final BehaviorSubject<LoginState> _stateController =
+      BehaviorSubject<LoginState>();
 
   Stream<String> get outEmail =>
       _emailController.stream.transform(validateEmail);
 
   Stream<String> get outPassword =>
       _passwordController.stream.transform(validatePassword);
+
+  Stream<LoginState> get outState => _stateController.stream;
 
   Stream<bool> get outSubmitValid =>
       Rx.combineLatest2(outEmail, outPassword, (a, b) => true);
@@ -19,10 +34,57 @@ class LoginBloc extends BlocBase with LoginValidators {
 
   Function(String) get changePassword => _passwordController.sink.add;
 
+  late final StreamSubscription<User?> _streamSubscription;
+
+  LoginBloc() {
+    _streamSubscription =
+        FirebaseAuth.instance.userChanges().listen((user) async {
+      if (user == null)
+        _stateController.add(LoginState.IDLE);
+      else if (await verifyPrivileges(user))
+        _stateController.add(LoginState.SUCCESS);
+      else {
+        FirebaseAuth.instance.signOut();
+        _stateController.add(LoginState.FAIL);
+      }
+    });
+  }
+
+  void submit() async {
+    final email = _emailController.value;
+    final password = _passwordController.value;
+
+    _stateController.add(LoginState.LOADING);
+
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    )
+        .catchError((e) {
+      _stateController.add(LoginState.FAIL);
+    });
+  }
+
+  Future<bool> verifyPrivileges(User user) async {
+    return await FirebaseFirestore.instance
+        .collection('admins')
+        .doc(user.uid)
+        .get()
+        .then((doc) {
+      return doc.data() == null ? false : true;
+    }).catchError((e) {
+      return false;
+    });
+  }
+
   @override
   void dispose() {
     _emailController.close();
     _passwordController.close();
+    _stateController.close();
+
+    _streamSubscription.cancel();
     super.dispose();
   }
 }
--- gerente_loja/lib/screens/login_screen.dart
+++ gerente_loja/lib/screens/login_screen.dart
@@ -1,74 +1,132 @@
 import 'package:flutter/material.dart';
 import 'package:gerente_loja/blocs/login_bloc.dart';
+import 'package:gerente_loja/screens/home_screen.dart';
 import 'package:gerente_loja/widgets/input_field.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final _loginBloc = LoginBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    _loginBloc.outState.listen((state) {
+      switch (state) {
+        case LoginState.SUCCESS:
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => HomeScreen(),
+            ),
+          );
+          break;
+        case LoginState.FAIL:
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Erro'),
+              content: Text('Você não possui os privilégios necessários'),
+            ),
+          );
+          break;
+        case LoginState.LOADING:
+        case LoginState.IDLE:
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _loginBloc.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: Container(
-        alignment: Alignment.center,
-        margin: EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Icon(
-                Icons.store_mall_directory,
-                color: Colors.pinkAccent,
-                size: 160,
-              ),
-              InputField(
-                icon: Icons.person_outline,
-                hint: 'Usuário',
-                obscure: false,
-                stream: _loginBloc.outEmail,
-                onChanged: _loginBloc.changeEmail,
-              ),
-              InputField(
-                icon: Icons.lock_outline,
-                hint: 'Senha',
-                obscure: true,
-                stream: _loginBloc.outPassword,
-                onChanged: _loginBloc.changePassword,
-              ),
-              SizedBox(height: 32),
-              StreamBuilder<bool>(
-                  stream: _loginBloc.outSubmitValid,
-                  builder: (context, snapshot) {
-                    bool data = snapshot.data ?? false;
-                    return SizedBox(
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: data ? () {} : null,
-                        child: Text(
-                          'Entrar',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
+      body: StreamBuilder<LoginState>(
+          stream: _loginBloc.outState,
+          initialData: LoginState.LOADING,
+          builder: (context, snapshot) {
+            switch (snapshot.data) {
+              case LoginState.LOADING:
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.pinkAccent,
+                  ),
+                );
+              case LoginState.FAIL:
+              case LoginState.SUCCESS:
+              case LoginState.IDLE:
+              default:
+                return Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Icon(
+                          Icons.store_mall_directory,
+                          color: Colors.pinkAccent,
+                          size: 160,
                         ),
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.resolveWith((Set states) {
-                            const Set interactiveStates = {
-                              MaterialState.disabled,
-                            };
-                            if (states.any(interactiveStates.contains)) {
-                              return Colors.pinkAccent.withAlpha(140);
-                            }
-                            return Colors.pinkAccent;
-                          }),
+                        InputField(
+                          icon: Icons.person_outline,
+                          hint: 'Usuário',
+                          obscure: false,
+                          stream: _loginBloc.outEmail,
+                          onChanged: _loginBloc.changeEmail,
                         ),
-                      ),
-                    );
-                  }),
-            ],
-          ),
-        ),
-      ),
+                        InputField(
+                          icon: Icons.lock_outline,
+                          hint: 'Senha',
+                          obscure: true,
+                          stream: _loginBloc.outPassword,
+                          onChanged: _loginBloc.changePassword,
+                        ),
+                        SizedBox(height: 32),
+                        StreamBuilder<bool>(
+                            stream: _loginBloc.outSubmitValid,
+                            builder: (context, snapshot) {
+                              bool data = snapshot.data ?? false;
+                              return SizedBox(
+                                height: 50,
+                                child: ElevatedButton(
+                                  onPressed: data ? _loginBloc.submit : null,
+                                  child: Text(
+                                    'Entrar',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.resolveWith(
+                                            (Set states) {
+                                      const Set interactiveStates = {
+                                        MaterialState.disabled,
+                                      };
+                                      if (states
+                                          .any(interactiveStates.contains)) {
+                                        return Colors.pinkAccent.withAlpha(140);
+                                      }
+                                      return Colors.pinkAccent;
+                                    }),
+                                  ),
+                                ),
+                              );
+                            }),
+                      ],
+                    ),
+                  ),
+                );
+            }
+          }),
     );
   }
 }
