e8c4017216ef632b122eb07c563e0d985eff0d80
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-07-01 19:08:58 -0300

    Updates gerente_loja projects

---
+++ gerente_loja/lib/blocs/user_bloc.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+
+class UserBloc extends BlocBase {
+  final BehaviorSubject<List<Map<String, dynamic>>> _usersController =
+      BehaviorSubject<List<Map<String, dynamic>>>();
+
+  final Map<String, Map<String, dynamic>> _users =
+      Map<String, Map<String, dynamic>>();
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  UserBloc() {
+    _addUsersListener();
+  }
+
+  void _addUsersListener() {
+    _firestore.collection('users').snapshots().listen((snapshot) {
+      snapshot.docChanges.forEach((docChange) {
+        String uid = docChange.doc.id;
+        Map<String, dynamic> data =
+            docChange.doc.data() ?? Map<String, dynamic>();
+        switch (docChange.type) {
+          case DocumentChangeType.added:
+            _users[uid] = data;
+            _subscribeToOrders(uid);
+            break;
+          case DocumentChangeType.modified:
+            _users[uid]!.addAll(data);
+            _usersController.add(_users.values.toList(growable: false));
+            break;
+          case DocumentChangeType.removed:
+            _users.remove(uid);
+            _unsubscribeToOrders(uid);
+            _usersController.add(_users.values.toList(growable: false));
+            break;
+        }
+      });
+    });
+  }
+
+  void _subscribeToOrders(String uid) {
+    _users[uid]!['subscription'] = _firestore
+        .collection('users')
+        .doc(uid)
+        .collection('orders')
+        .snapshots()
+        .listen((orders) async {
+      int numOrders = orders.docs.length;
+
+      double money = 0.0;
+
+      for (DocumentSnapshot d in orders.docs) {
+        DocumentSnapshot<Map<String, dynamic>> order =
+            await _firestore.collection('orders').doc(d.id).get();
+        money += order.data()!['totalPrice'];
+      }
+
+      _users[uid]!.addAll({
+        'money': money,
+        'orders': numOrders,
+      });
+
+      _usersController.add(_users.values.toList(growable: false));
+    });
+  }
+
+  void _unsubscribeToOrders(String uid) {
+    StreamSubscription<QuerySnapshot<Map<String, dynamic>>> subscription =
+        _users[uid]!['subscription'];
+    subscription.cancel();
+  }
+
+  @override
+  void dispose() {
+    _usersController.close();
+  }
+}
